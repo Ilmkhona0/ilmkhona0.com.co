d2022d3cee420076cc734166ffe97dc2142e7f67
Ilmkhona0 <ilmkhona@gmail.com>
2026-02-26 00:47:07 +0100

    Add null check for _mongoClientPromise
    Ensure _mongoClientPromise is defined before assignment.

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,12 +4,13 @@ const uri = process.env.MONGODB_URI!;
 const options = {};
 
 declare global {
-  // eslint-disable-next-line no-var
+   
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
+
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please add your MongoDB URI to .env.local');
@@ -20,10 +21,12 @@ if (process.env.NODE_ENV === 'development') {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = global._mongoClientPromise!;
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
 export default clientPromise;
+
+
