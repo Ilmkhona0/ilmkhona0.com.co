93d8c219a63c2c57f50322dd1775e2a88799cd45
Muhammad <ilmkhona@gmail.com>
2026-02-26 03:57:03 +0100

    Fix updated

--- app/api/admin/update/route.ts
+++ app/api/admin/update/route.ts
@@ -1,4 +1,6 @@
+import { NextResponse } from "next/server";
 export async function PUT(req: any) {
 // MongoDB disabled
 return NextResponse.json({ message: 'Database disabled for deployment' });
+
 }
