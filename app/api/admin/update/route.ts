dca59298d4718c4fd9f8a0ef28732ecfbf85fe0c
Muhammad <ilmkhona@gmail.com>
2026-02-27 01:25:52 +0100

    Fix update

--- app/api/admin/update/route.ts
+++ app/api/admin/update/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import clientPromise from "../../../lib/mongodb";
+import clientPromise from "../../../../lib/mongodb";
 import { MongoClient } from "mongodb";
 
 export async function PUT(req: any) {
